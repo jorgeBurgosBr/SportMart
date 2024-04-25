b46ded329176f644f47ec8726fcc5364a6dddd6a
Pablo <pablonaranjocid@gmail.com>
2024-04-25 08:22:36 +0200

    creado el boceto de la home de running

---
+++ php/bbdd.php
@@ -0,0 +1,123 @@
+<?php
+
+require_once 'conecta.php';
+
+function existeBBDD()
+{
+   $bd = new BaseDeDatos();
+   $flag = false;
+   try {
+      if ($bd->conectar()) {
+         $conexion = $bd->getConexion();
+         $sql = "SHOW DATABASE LIKE 'SportMart'";
+         $result = mysqli_query($conexion, $sql) or die(mysqli_error($conexion));
+         if (mysqli_num_rows($result) > 0) {
+            $flag = true;
+         }
+      }
+   } catch (Exception $e) {
+      echo $e->getMessage();
+   }
+   $bd->cerrar();
+   return $flag;
+}
+
+function crearBD()
+{
+   $bd = new BaseDeDatos();
+   try {
+      if ($bd->conectar()) {
+         $conexion =  $bd->getConexion();
+         $createSql = [
+            "CREATE DATABASE  SportMart;",
+            "USE SportMart;",
+            "CREATE TABLE CLIENTE (
+               id_cliente INT AUTO_INCREMENT,
+               nombre VARCHAR(255) NOT NULL,
+               apellidos VARCHAR(255) NOT NULL,
+               correo VARCHAR(255) NOT NULL,
+               telefono VARCHAR(255) NOT NULL,
+               PRIMARY KEY (id_cliente)
+               );",
+            "CREATE TABLE PRODUCTO (
+               id_producto INT AUTO_INCREMENT,
+               nombre VARCHAR(255) NOT NULL,
+               descripcion TEXT,
+               precio DECIMAL(10, 2) NOT NULL,
+               imagen VARCHAR(255),
+               talla VARCHAR(255),
+               sexo ENUM('Hombre', 'Mujer') NOT NULL,
+               PRIMARY KEY (id_producto)
+               );",
+            "CREATE TABLE FAVORITOS (
+               id_favorito INT AUTO_INCREMET,
+               id_cliente INT,
+               id_producto INT,
+               PRIMARY KEY (id_favorito),
+               FOREIGN KEY (id_cliente) REFERENCES CLIENTE(id_cliente),
+               FOREIGN KEY (id_producto) REFERENCES PRODUCTO(id_producto)
+                  );",
+            "CREATE TABLE CARRITO (
+               id_cliente INT PRIMARY KEY,
+               id_producto INT,
+               cantidad INT NOT NULL,
+               FOREIGN KEY (id_producto) REFERENCES PRODUCTO(id_producto),
+               FOREIGN KEY (id_cliente) REFERENCES CLIENTE(id_cliente)
+               );",
+            "CREATE TABLE CATEGORIA (
+               id_categoria INT  AUTO_INCREMENT, 
+               categoria VARCHAR(150) UNIQUE,
+               id_producto INT,
+               PRIMARY KEY (id_categoria),
+               FOREIGN KEY (id_producto) REFERENCES PRODUCTO(id_producto)
+               );",
+            "CREATE TABLE DETALLES_PEDIDO (
+               id_pedido INT AUTO_INCREMENT,
+               id_producto INT NOT NULL,
+               cantidad INT  NOT NULL,
+               PRIMARY KEY (id_pedido)
+            );",
+            "CREATE TABLE DIRECCION_ENVIO (
+               id_pedido INT PRIMARY KEY,
+               direccion TEXT NOT NULL,
+               FOREIGN KEY (id_pedido) REFERENCES DETALLES_PEDIDO(id_pedido)
+               );",
+            "CREATE TABLE METODO_PAGO (
+               id_pedido INT PRIMARY KEY,
+               n_tarjeta VARCHAR(19) NOT NULL,
+               fecha DATE NOT NULL,
+               nombre VARCHAR(225) NOT NULL,
+               FOREIGN KEY (id_pedido) REFERENCES DETALLES_PEDIDO(id_pedido)
+               );"
+         ];
+         $insertSql = [];
+         // Ejecutar las consultas de inserción de datos
+         ejecutarSentencias($conexion, $createSql);
+         ejecutarSentencias($conexion, $insertSql);
+         $bd->cerrar();
+      }
+   } catch (Exception $e) {
+      echo $e->getMessage();
+   }
+}
+function ejecutarSentencias($conexion, $sentencias)
+{
+   foreach ($sentencias as $sql) {
+      if (mysqli_multi_query($conexion, $sql)) {
+         do {
+            // Almacenar primer resultado set (si existe)
+            if ($result = mysqli_store_result($conexion)) {
+               mysqli_free_result($result);
+            }
+            // Verificar si hay más resultados
+         } while (mysqli_next_result($conexion));
+      } else {
+         echo "Error ejecutando SQL: " . mysqli_error($conexion);
+         break; // Salir del bucle en caso de error
+      }
+   }
+}
+// Crea tablas e inserta datos solo si no existe Ambulatorio
+if (!existeBBDD()) {
+   crearBD();
+}
