1401b23b8eaa4287d4dc73231fffd537eba31edc
Jburgos96 <jorge.burgos.barrera@gmail.com>
2024-05-04 12:10:22 +0200

    Cambiado que mire el ultimo id que haya, y que asocie el siguiente que corresponde

--- php/procesar_registro.php
+++ php/procesar_registro.php
@@ -6,7 +6,6 @@ $bd = new BaseDeDatos();
 if ($bd->conectar()) {
    $conn = $bd->getConexion();
    $bd->seleccionarContexto('sportmart');
-   // echo "se ha conectado perfectamente";
 
    if ($_SERVER["REQUEST_METHOD"] == "POST") {
       $respuesta = [
@@ -20,27 +19,33 @@ if ($bd->conectar()) {
       $password = mysqli_real_escape_string($conn, $_POST['password']);
       $contrasena_hash = password_hash($password, PASSWORD_DEFAULT);
 
+      // Generar el ID del cliente
+      $ultimo_id_query = mysqli_query($conn, "SELECT MAX(id_cliente) AS max_id FROM cliente");
+      $ultimo_id_result = mysqli_fetch_assoc($ultimo_id_query);
+      $nuevo_id = $ultimo_id_result['max_id'] + 1;
+
       // Comprobar si el usuario ya existe en la base de datos
       $sql = mysqli_query($conn, "SELECT * FROM cliente WHERE correo = '$correo'");
       if (mysqli_num_rows($sql) > 0) {
          $respuesta['success'] = false;
          $respuesta['error'] = 'El correo ya está registrado';
       } else {
-         //Insertamos los datos en la BBDD
-         $sql2 = mysqli_query($conn, "INSERT INTO cliente (nombre, apellidos, correo, telefono, password) 
-                                    VALUES ('$nombre', '$apellidos', '$correo', '$telefono', '$contrasena_hash')");
+         // Insertar los datos en la BBDD con el nuevo ID generado
+         $sql2 = mysqli_query($conn, "INSERT INTO cliente (id_cliente, nombre, apellidos, correo, telefono, password) 
+                                    VALUES ('$nuevo_id', '$nombre', '$apellidos', '$correo', '$telefono', '$contrasena_hash')");
 
+                                    
          // // Recojo el id del cliente
          // $row = mysqli_fetch_assoc($sql3);
          // $id_original = $row['id_cliente'];
 
-
          if ($sql2) { 
             $respuesta['success'] = true;
-            // $_SESSION['id_cliente'] = $id_original;
+            // $_SESSION['id_cliente'] = $nuevo_id;
          }
       }
       header('Content-Type: application/json');
       echo json_encode($respuesta);
    }
 }
+?>
